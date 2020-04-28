6eca79bb3ed640b66ea8d04c373d13aa03d2dfdb
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-28 22:36:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,8 @@ row_index = 0
 while row_index < array_of_arrays.count do
   element_index = 0
   while element_index < array_of_arrays[row_index].count do
-    p array_of_arrays[row_index][element_index] % 2 == 0
+    if array_of_arrays[row_index][element_index] %2 ==0
+      p array_of_arrays[row_index][element_index]
     element_index += 1
   end
   row_index+=1
